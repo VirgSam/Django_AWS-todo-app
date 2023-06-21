536a61aa885f2e34cbdb10362b15c2fb22d61e54
chris01 <chris_odum@yahoo.com>
2023-06-21 13:49:47 +0200

    Update settings.py

--- todo_project/todo_project/mysite/settings.py
+++ todo_project/todo_project/mysite/settings.py
@@ -20,7 +20,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "  "
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
